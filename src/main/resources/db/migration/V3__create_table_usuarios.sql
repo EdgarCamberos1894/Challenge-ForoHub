290234a43ef5629a77580f11a0378d87603bc4ed
EdgarCamberos1894 <edgarcamberos18@gmail.com>
2024-06-11 21:51:17 -0700

    fix(database): Remove the 'perfil_id' column from the 'tabla'
    Remove the 'perfil_id' column from the 'tabla' table to correct database schema.

--- src/main/resources/db/migration/V3__create_table_usuarios.sql
+++ src/main/resources/db/migration/V3__create_table_usuarios.sql
@@ -2,7 +2,5 @@ CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(255) NOT NULL,
     correo_electronico VARCHAR(255) NOT NULL,
-    contrasena VARCHAR(255) NOT NULL,
-    perfil_id INT,
-    FOREIGN KEY (perfil_id) REFERENCES perfiles(id)
+    contrasena VARCHAR(255) NOT NULL
 );
